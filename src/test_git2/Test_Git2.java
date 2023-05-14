8e9cfe0875eb7b7d112433b6fdbc5d13e8721775
nguyenziet <nguyenviettt5@gmail.com>
2023-05-14 17:14:47 +0700

    add project

---
+++ src/test_git2/Test_Git2.java
@@ -0,0 +1,17 @@
+package test_git2;
+
+/**
+ *
+ * @author Hello
+ */
+public class Test_Git2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!");
+    }
+    
+}
